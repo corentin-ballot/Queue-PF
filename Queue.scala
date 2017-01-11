ce5dc85ace2aa379de35ea360e2f998cbf1ad1ce
Corentin Ballot <corentin-ballot@users.noreply.github.com>
2017-01-11 15:51:52 +0100

    Edit dequeue method

--- Queue.scala
+++ Queue.scala
@@ -1,17 +1,16 @@
 /** Une classe liste FIFO. */
 case class Queue[T](in:List[T] = Nil, out:List[T] = Nil) {
 
-	/** Ajoute un élément `x` en tte. */
+	/** Ajoute un élément `x` en tête. */
 	def enqueue(x:T):Queue[T] = {
 		Queue(x :: in, out)
 	}
 
 	/** Retire le dernier élément. */
 	def dequeue():(T,Queue[T]) = in match {
-		// in.head est null dans ce cas, il faut le remplacer par NULL 
-		case Nil if(out.isEmpty) => (in.head, Queue(Nil, Nil))
+		case Nil if(out.isEmpty) => throw new Exception("La queue est vide.")
 		case Nil => (out.head, Queue(Nil, out.tail))
-		case _ if(out.isEmpty) => (in.reverse.head, Queue(Nil, in.reverse.tail))
+		case _ if(out.isEmpty) => {val tmp = in.reverse; (tmp.head, Queue(Nil, tmp.tail))}
 		case _ => (out.head, Queue(in, out.tail))		
 	}
 
